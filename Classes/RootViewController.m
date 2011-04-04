aba40fa5638fdb6de512669c1eac185ec192b5c9
stephandev <stephandeveloper@gmail.com>
2011-04-04 19:47:38 +0200

    fixed some spelling

--- Classes/RootViewController.m
+++ Classes/RootViewController.m
@@ -49,7 +49,7 @@
 	[newsTable reloadData];
 	UIAlertView *alert = [[UIAlertView alloc]
 						  initWithTitle:@"Impressum" // oder einfach Wilkommen in der Touch-Mania.com Applikation?
-						  message:@"Diese App ist ein Community-Projekt und steht unter der Lizenz GPL v2. Entwickelt wurde sie mit freundlicher Unterstützung der Apfeltalk GmbH, Breitmoss 46, 83334 Inzell. \n\nFür die Richtigkeit der Inhalte wird nicht gehaftet. \n\nDie Forenzugriffs-API stammt von Tapatalk."
+						  message:@"Diese App ist ein Community-Projekt und steht unter der Lizenz GPL v2. Entwickelt wurde sie mit freundlicher Unterstützung der Apfeltalk GmbH, Breitmoos 46, 83334 Inzell. \n\nFür die Richtigkeit der Inhalte wird nicht gehaftet. \n\nDie Forenzugriffs-API stammt von Tapatalk."
 						  delegate:self
 						  cancelButtonTitle:@"OK"
 						  otherButtonTitles:@"Kontakt"
